05b70a56a04073af36d7fe9cd47d65c0a2051b02
virginie <meliana.322@gmail.com>
2022-03-08 10:12:04 +0100

    inputs first, last, email, birthdate, quantity ok

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -46,47 +46,71 @@ const loc6 = document.getElementById("location6");
 const participation = document.getElementById("quantity");
 const valideConditions = document.getElementById("checkbox1");
 
-// const dateActuelle = new Date();
-// const dateActuelleLocale = dateActuelle.toLocaleDateString("fr-FR", {
-//   day: "numeric",
-//   month: "numeric",
-//   year: "numeric",
-// });
-// const dateNaissance = dateActuelleLocale[2] > 18;
+//  const dateActuelle = new Date();
+//  const dateActuelleLocale = dateActuelle.toLocaleDateString("fr-FR", {
+//    day: "numeric",
+//    month: "numeric",
+//    year: "numeric",
+//  });
+//  const dateNaissance = dateActuelleLocale[2] > 18;
 
 // Confirmation de la soumission réussie
-document.getElementById("formulaire").addEventListener("input", function (e) {
+document.getElementById("formulaire").addEventListener("submit", function (e) {
   e.preventDefault();
-
-  let error;
+  let error = "";
 
   if (firstName.value.length < 2) {
     error = "Merci de renseigner le champ";
     formData[0].setAttribute("data-error", error);
     formData[0].setAttribute("data-error-visible", true);
-  }
-  if (lastName.value.length < 2) {
-    error = "Merci de renseigner le champ";
-    formData[1].setAttribute("data-error", error);
-    formData[1].setAttribute("data-error-visible", true);
-  }
-  if (!eMail.value.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) {
-    error = "Merci de saisir une adresse mail valide";
-    formData[2].setAttribute("data-error", error);
-    formData[2].setAttribute("data-error-visible", true);
-  }
-  if (birthDate.value < 8 || dateNaissance.value) {
-    error = "Merci de renseigner le champ";
-    formData[3].setAttribute("data-error", error); // creation de l'objet avec attribut data-error
-    formData[3].setAttribute("data-error-visible", true);
   } else {
     formData[0].setAttribute("data-error-visible", false);
   }
 
-  if (error) {
-    document.getElementById("error").innerHTML = error;
-    return false;
+  if (lastName.value.length < 2) {
+    error = "Merci de renseigner le champ";
+    formData[1].setAttribute("data-error", error);
+    formData[1].setAttribute("data-error-visible", true);
   } else {
+    formData[1].setAttribute("data-error-visible", false);
+  }
+
+  if (!eMail.value.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,64})+$/)) {
+    error = "Merci de saisir une adresse mail valide";
+    formData[2].setAttribute("data-error", error);
+    formData[2].setAttribute("data-error-visible", true);
+  } else {
+    formData[2].setAttribute("data-error-visible", false);
+  }
+
+  const today = new Date();
+  const selectedDate = new Date(birthDate.value);
+
+  console.log(birthDate.value);
+  if (birthDate.value === "" || today < selectedDate) {
+    error = "Merci de renseigner le champ";
+    formData[3].setAttribute("data-error", error);
+    formData[3].setAttribute("data-error-visible", true);
+  } else {
+    formData[3].setAttribute("data-error-visible", false);
+  }
+  //console.log(typeof quantityTournament.value);
+  if (quantityTournament.value === "" || Number(quantityTournament.value) < 0) {
+    error = "Merci de renseigner le champ";
+    formData[4].setAttribute("data-error", error);
+    formData[4].setAttribute("data-error-visible", true);
+  } else {
+    formData[4].setAttribute("data-error-visible", false);
+  }
+  if (quantityTournament.value === "" || Number(quantityTournament.value) < 0) {
+    error = "Merci de renseigner le champ";
+    formData[4].setAttribute("data-error", error);
+    formData[4].setAttribute("data-error-visible", true);
+  } else {
+    formData[4].setAttribute("data-error-visible", false);
+  }
+
+  if (error === "") {
     alert("Merci ! Votre réservation a été reçue.");
   }
 });
